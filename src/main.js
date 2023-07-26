e12e903c5454609359c28cddc2d1548d03aa9483
sam rubarema <samrubarema6@gmail.com>
2023-07-26 17:06:56 +0300

    removed an used library

--- src/main.js
+++ src/main.js
@@ -4,12 +4,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store/employees'
-import Vuelidate from '@vuelidate/core';
 
 const app = createApp(App)
 
 app.use(router)
 app.use(store)
-app.use(Vuelidate);
 
 app.mount('#app')
