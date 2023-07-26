f77272e644a58ef1cefdf6b843f815b1be4568b8
sam rubarema <samrubarema6@gmail.com>
2023-07-26 16:59:38 +0300

    update title of the project

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template>
   <header>
-    <h1>ONLINE SQL QUERY EXECUTOR</h1>
+    <h1>ONLINE SQL QUERY RUNNER</h1>
     <div class="wrapper">
       <nav>
         <RouterLink to="/">ExquteQuery</RouterLink>
